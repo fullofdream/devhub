fullofdream/devhub

commit a392bfb8273ef88afd4d2054bb8c9d44b1851125

Improved column separator handling
Specially on modal transitions

packages/components/src/components/columns/Column.tsx
@@ -1,9 +1,8 @@
-import React, { ReactNode, useRef, useState } from 'react'
+import React, { ReactNode, useRef } from 'react'
 import { StyleProp, StyleSheet, View, ViewProps, ViewStyle } from 'react-native'
 
 import { useCSSVariablesOrSpringAnimatedTheme } from '../../hooks/use-css-variables-or-spring--animated-theme'
 import { useEmitter } from '../../hooks/use-emitter'
-import { Platform } from '../../libs/platform'
 import { contentPadding } from '../../styles/variables'
 import { SpringAnimatedView } from '../animated/spring/SpringAnimatedView'
 import { Separator, separatorTickSize } from '../common/Separator'
@@ -15,43 +14,52 @@ export const columnMargin = contentPadding / 2
 export interface ColumnProps extends ViewProps {
   children?: ReactNode
   columnId: string
+  fullWidth?: boolean
   pagingEnabled?: boolean
+  renderSideSeparators?: boolean
   style?: StyleProp<ViewStyle>
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: 'row',
-    height: '100%',
-  },
-})
-
 export const Column = React.memo(
   React.forwardRef((props: ColumnProps, ref) => {
-    const { children, columnId, pagingEnabled, style, ...otherProps } = props
+    const {
+      children,
+      columnId,
+      fullWidth,
+      pagingEnabled,
+      renderSideSeparators,
+      style,
+      ...otherProps
+    } = props
 
     const _columnRef = useRef<View>(null)
     const columnRef = (ref as React.RefObject<View>) || _columnRef
-    const [showFocusBorder, setShowFocusBorder] = useState(false)
+
+    const columnBorderRef = useRef<View>(null)
+
     const { sizename } = useAppLayout()
     const springAnimatedTheme = useCSSVariablesOrSpringAnimatedTheme()
-    const width = useColumnWidth()
+    const columnWidth = useColumnWidth()
 
     useEmitter(
       'FOCUS_ON_COLUMN',
       (payload: { columnId: string; highlight?: boolean }) => {
-        if (!(payload.columnId && payload.columnId === columnId)) return
+        if (!columnBorderRef.current) return
+
+        if (!(payload.columnId && payload.columnId === columnId)) {
+          columnBorderRef.current.setNativeProps({ style: { opacity: 0 } })
+          return
+        }
 
         if (payload.highlight) {
-          setShowFocusBorder(true)
+          columnBorderRef.current.setNativeProps({ style: { opacity: 1 } })
           setTimeout(() => {
-            setShowFocusBorder(false)
+            if (!columnBorderRef.current) return
+            columnBorderRef.current.setNativeProps({ style: { opacity: 0 } })
           }, 1000)
         }
 
-        if (Platform.OS === 'web' && columnRef.current) {
-          columnRef.current.focus()
-        }
+        if (columnRef.current) columnRef.current.focus()
       },
     )
 
@@ -62,34 +70,39 @@ export const Column = React.memo(
         key={`column-inner-${columnId}`}
         className={pagingEnabled ? 'snap-item-start' : ''}
         style={[
-          styles.container,
           {
+            flexDirection: 'row',
+            height: '100%',
             backgroundColor: springAnimatedTheme.backgroundColor,
-            width,
           },
+          fullWidth ? { flex: 1 } : { width: columnWidth },
           style,
         ]}
       >
-        <Separator half horizontal={false} thick={sizename > '1-small'} />
-        <View style={{ flex: 1 }}>{children}</View>
-        <Separator half horizontal={false} thick={sizename > '1-small'} />
-
-        {!!showFocusBorder && (
-          <SpringAnimatedView
-            collapsable={false}
-            pointerEvents="box-none"
-            style={[
-              {
-                ...StyleSheet.absoluteFillObject,
-                borderWidth: 0,
-                borderRightWidth: Math.max(4, separatorTickSize),
-                borderLeftWidth: Math.max(4, separatorTickSize),
-                borderColor: springAnimatedTheme.foregroundColorMuted50,
-                zIndex: 1000,
-              },
-            ]}
-          />
+        {!!renderSideSeparators && (
+          <Separator half horizontal={false} thick={sizename > '1-small'} />
         )}
+        <View style={{ flex: 1 }}>{children}</View>
+        {!!renderSideSeparators && (
+          <Separator half horizontal={false} thick={sizename > '1-small'} />
+        )}
+
+        <SpringAnimatedView
+          ref={columnBorderRef}
+          collapsable={false}
+          pointerEvents="box-none"
+          style={[
+            StyleSheet.absoluteFill,
+            {
+              borderWidth: 0,
+              borderRightWidth: Math.max(4, separatorTickSize),
+              borderLeftWidth: Math.max(4, separatorTickSize),
+              borderColor: springAnimatedTheme.foregroundColorMuted50,
+              zIndex: 1000,
+              opacity: 0,
+            },
+          ]}
+        />
       </SpringAnimatedView>
     )
   }),

packages/components/src/components/columns/Columns.tsx
@@ -11,6 +11,7 @@ import { Omit } from '@devhub/core'
 import { ColumnContainer } from '../../containers/ColumnContainer'
 import { useEmitter } from '../../hooks/use-emitter'
 import { bugsnag } from '../../libs/bugsnag'
+import { separatorTickSize } from '../common/Separator'
 import { useColumnWidth } from '../context/ColumnWidthContext'
 import { useAppLayout } from '../context/LayoutContext'
 
@@ -28,6 +29,7 @@ const styles = StyleSheet.create({
   },
   flatlist: {
     flex: 1,
+    marginHorizontal: -separatorTickSize / 2,
   },
 })
 

packages/components/src/components/columns/EventOrNotificationColumn.tsx
@@ -146,7 +146,11 @@ export const EventOrNotificationColumn = React.memo(
     )
 
     return (
-      <Column columnId={column.id} pagingEnabled={pagingEnabled}>
+      <Column
+        columnId={column.id}
+        pagingEnabled={pagingEnabled}
+        renderSideSeparators
+      >
         <ColumnHeader>
           <ColumnHeaderItem
             analyticsLabel={undefined}

packages/components/src/components/modals/ModalRenderer.tsx
@@ -145,6 +145,23 @@ export function ModalRenderer(props: ModalRendererProps) {
     },
   )
 
+  const separatorTransitions = useTransition<string, any>(
+    renderSeparator && sizename !== '1-small' && modalStack.length
+      ? [modalStack[0].name]
+      : [],
+    item => `modal-separator-${item}`,
+    {
+      reset: true,
+      unique: true,
+      config: { ...config.default, precision: 1 },
+      immediate,
+      from: { right: size },
+      enter: { right: 0 },
+      update: { right: 0 },
+      leave: { right: size + separatorTickSize },
+    },
+  )
+
   return (
     <>
       {!!overlayTransition && (
@@ -173,7 +190,7 @@ export function ModalRenderer(props: ModalRendererProps) {
       )}
 
       {!!modalTransitions.length && (
-        <SpringAnimatedView
+        <View
           collapsable={false}
           style={{
             position: 'absolute',
@@ -185,8 +202,42 @@ export function ModalRenderer(props: ModalRendererProps) {
             zIndex: 900,
           }}
         >
-          {modalTransitions.map(
-            ({ key, item, props: { width, ...animatedStyle } }) =>
+          <View
+            collapsable={false}
+            style={{
+              position: 'absolute',
+              top: 0,
+              bottom: 0,
+              left: 0,
+              width: columnWidth,
+              overflow: 'hidden',
+              zIndex: 900,
+            }}
+          >
+            {modalTransitions.map(
+              ({ key, item, props: { width, ...animatedStyle } }) =>
+                !!item && (
+                  <SpringAnimatedView
+                    key={key}
+                    collapsable={false}
+                    style={{
+                      position: 'absolute',
+                      top: 0,
+                      bottom: 0,
+                      flexDirection: 'row',
+                      ...animatedStyle,
+                      overflow: 'hidden',
+                      zIndex: 900 + item.index,
+                    }}
+                  >
+                    {renderModal(item)}
+                  </SpringAnimatedView>
+                ),
+            )}
+          </View>
+
+          {separatorTransitions.map(
+            ({ key, item, props: animatedStyle }) =>
               !!item && (
                 <SpringAnimatedView
                   key={key}
@@ -195,32 +246,14 @@ export function ModalRenderer(props: ModalRendererProps) {
                     position: 'absolute',
                     top: 0,
                     bottom: 0,
-                    flexDirection: 'row',
                     ...animatedStyle,
-                    overflow: 'hidden',
-                    zIndex: 900 + item.index,
                   }}
                 >
-                  {renderModal(item)}
-
-                  {!!renderSeparator && (
-                    <View
-                      collapsable={false}
-                      style={{
-                        position: 'absolute',
-                        top: 0,
-                        bottom: 0,
-                        right: 0,
-                        zIndex: 1000,
-                      }}
-                    >
-                      <Separator thick zIndex={1000} />
-                    </View>
-                  )}
+                  <Separator thick />
                 </SpringAnimatedView>
               ),
           )}
-        </SpringAnimatedView>
+        </View>
       )}
     </>
   )

packages/components/src/screens/MainScreen.tsx
@@ -337,7 +337,6 @@ export const MainScreen = React.memo(() => {
         />
 
         <Separator
-          half
           horizontal={horizontalSidebar}
           thick={!horizontalSidebar}
           zIndex={1000}

commit 013df053472c6a95e11adcc8addeae56cacb753e

[Mobile] Fix tap bug regression
Caused by https://github.com/devhubapp/devhub/commit/a392bfb8273ef88afd4d2054bb8c9d44b1851125#diff-6bd889a62a1ccf9e71b08df0a789cd19

cc #123

packages/components/src/components/columns/Column.tsx
@@ -3,6 +3,7 @@ import { StyleProp, StyleSheet, View, ViewProps, ViewStyle } from 'react-native'
 
 import { useCSSVariablesOrSpringAnimatedTheme } from '../../hooks/use-css-variables-or-spring--animated-theme'
 import { useEmitter } from '../../hooks/use-emitter'
+import { Platform } from '../../libs/platform'
 import { contentPadding } from '../../styles/variables'
 import { SpringAnimatedView } from '../animated/spring/SpringAnimatedView'
 import { Separator, separatorTickSize } from '../common/Separator'
@@ -59,7 +60,9 @@ export const Column = React.memo(
           }, 1000)
         }
 
-        if (columnRef.current) columnRef.current.focus()
+        if (Platform.OS === 'web' && columnRef.current) {
+          columnRef.current.focus()
+        }
       },
     )
 